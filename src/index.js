b0dc7f4be542bd1be13286f53914a0d7ca9aee8a
龙邦 <antietie@qq.com>
2020-07-10 10:24:32 +0800

    添加图片加载失败后显示默认图片方法

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ class Cc
     this.this__ = this_;
     return this;
   }
+
+  imgError(event){
+    let img = event.srcElement;
+    img.src = require('@/assets/img/load/load_picture_border1@1.png');
+    img.onerror = null;
+  }
 }
 
 export default Cc;
